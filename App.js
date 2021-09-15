b054dc7e8b9eab4a7574b89cba4f8c29f0225a0c
danhenrik <danihtoledo22@gmail.com>
2021-09-15 15:02:52 -0300

    Realm prototype
    Co-authored-by: Gabriel Teixeira <gabrielmteixeira@users.noreply.github.com>
    Co-authored-by: Ricardo Furbino <fubininho@users.noreply.github.com>

--- App.js
+++ App.js
@@ -1,6 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Realm from "realm";
+
+
+const Person = {
+  name: "Person",
+  primaryKey: "_id",
+  properties: {
+    _id: "objectId",
+    name: "string",
+    image: "string"
+  },
+  required: [
+    _id,
+    name,
+    image
+  ],
+};
+
+// Cria o banco
+const realm = await Realm.open({
+  //? Entra outros schemas
+  schema: [Person],
+});
+
+// imageName = nome da imagem de teste
+const imageName;
+
+// C - Create
+// Provável que _id tem como gerar automaticamente pelo realm, se n tiver nanoid
+const newPerson = realm.create("Person",{
+  name: "Daniel",
+  image: imageName,
+});
+
+// realm.write(() => {newPerson});
+realm.write(newPerson);
+
+// R - Read
+// ! OUT NISSO
+const people = realm.objects("Person");
+const person = realm.objectForPrimaryKey('Person', item_id);
+
+
+// U - Update
+
+
+
+// D - Delete
+
 
 export default function App() {
   return (
